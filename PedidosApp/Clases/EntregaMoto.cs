aea91735e0bd3f172cef800ec2c67b1c7ccc8ac1
NadiaCiro <141196587+NadiaCiro@users.noreply.github.com>
2025-04-23 15:15:07 -0500

    Agregar Interfaz y Clases

---
+++ PedidosApp/Clases/EntregaMoto.cs
@@ -0,0 +1,15 @@
+﻿using PedidosApp.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosApp.Clases
+{
+    public class EntregaMoto : IMetodoEntrega
+    {
+        public double CalcularCosto(int km) => 10 * km;
+        public string TipoEntrega() => "motocicleta";
+    }
+}
